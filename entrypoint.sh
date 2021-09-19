e6182175b94f3809b5e9b28a2897c9fe44b1d01d
leoppro <zhaoyilin@pingcap.com>
2021-09-19 18:39:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
 # avoid Permissions too open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
+printf "$7"
+
 echo 'ssh start'
 
 ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
@@ -20,7 +22,7 @@ echo 'sftp start'
 # create a temporary file containing sftp commands
 printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
 
 echo 'deploy success'
 exit 0
